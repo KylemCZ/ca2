31baa1f9d8cdf29824a8a49c3b044c43c2e38e9b
Jakub <jakuuuba@gmail.com>
2026-03-01 13:49:43 +0100

    fix

--- app/components/addProjectCard.tsx
+++ app/components/addProjectCard.tsx
@@ -5,7 +5,7 @@ export default function Add({ id }: { id: string }) {
         <section className='p-5 min-h-120 rounded-xl bg-gray-800 flex items-center justify-center hover:bg-gray-700 transition-colors duration-100'>
           <Link href={`/technologies/${id}/add`}
             className={"h-full w-full items-center justify-center flex"}>
-            <div className="text-9xl leading-none" style={{transform: 'translateY(-0.1em)'}}>
+            <div className="text-9xl" style={{transform: 'translateY(-0.1em)'}}>
               +
             </div>
           </Link>
--- app/components/addTechnologyCard.tsx
+++ app/components/addTechnologyCard.tsx
@@ -5,7 +5,7 @@ export default function Add() {
         <section className='p-5 min-h-120 rounded-xl bg-gray-800 flex items-center justify-center hover:bg-gray-700 transition-colors duration-100'>
           <Link href={"/technologies/add"}
             className={"h-full w-full items-center justify-center flex"}>
-            <div className="text-9xl leading-none" style={{transform: 'translateY(-0.1em)'}}>
+            <div className="text-9xl">
               +
             </div>
           </Link>
